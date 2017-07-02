165f3c19e8f44e0ee969cd87b7508f1ad7a1777e
Spencer Kordecki <spencerkordecki@gmail.com>
2017-07-02 10:34:23 -0400

    closes issue #2

--- index.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  var colors = ['#1abc9c', '#27ae60', '#3498db', '#8e44ad', '#2c3e50',
-  '#f1c40f', '#d35400', '#7f8c8d'];
-
-  $('#shuffler span i').on('click', function(){
-    var random = colors[Math.floor(Math.random() * colors.length)];
-    $('body').css('background', random);
-  });
-});
